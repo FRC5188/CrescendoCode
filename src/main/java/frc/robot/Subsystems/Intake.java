1db10990d911544756799d896d79b88312d43739
collinkn <collinkn@rose-hulman.edu>
2024-01-18 19:42:05 -0500

    solved issue CRES-30

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,15 +1,42 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    public enum IntPos {
-        Ground, Source, Middle, High
+    public enum IntakePosition {
+        SourcePickup, GroundPickup, Stowed, AmpScore
     }
-    protected IntPos _curPos;
-    public IntPos getIntakePosition() {
-            return this._curPos;
+    protected final double INTAKE_GROUND_PICKUP = 0; //subject to change. Hi programmer!
+    protected final double INTAKE_SOURCE_PICKUP = 0; //also subject to change!
+    protected final double INTAKE_STOWED = 0; //subject to change. Hi programmer!
+    protected final double INTAKE_AMP_SCORE = 0; //also subject to change!
+    protected IntakePosition _intakePosition;
+    protected PIDController _pivotMotorPID;
+    protected DutyCycleEncoder _pivotAbsEncoder;
+    public IntakePosition getIntakePosition() {
+            return this._intakePosition;
     }
-
-
+    public void setIntakePosition(IntakePosition dontKillMeZoe) {
+        _intakePosition = dontKillMeZoe;
+        switch (_intakePosition) {
+            case GroundPickup:
+                _pivotMotorPID.setSetpoint(INTAKE_GROUND_PICKUP);
+                break;
+            case SourcePickup:
+                _pivotMotorPID.setSetpoint(INTAKE_SOURCE_PICKUP);
+                break;
+            case Stowed:
+                _pivotMotorPID.setSetpoint(INTAKE_STOWED);
+                break;
+            case AmpScore:
+                _pivotMotorPID.setSetpoint(INTAKE_AMP_SCORE);
+                break;
+        }
+    }
+    public double getPivotMotorAngle() {
+        return this._pivotAbsEncoder.getAbsolutePosition();
+    }
 }
+

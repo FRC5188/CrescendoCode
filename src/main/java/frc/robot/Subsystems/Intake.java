182dcc45315c98aaea8d5aab75b3f6289bfc41cd
collinkn <collinkn@rose-hulman.edu>
2024-01-20 13:08:22 -0500

    aquire speed and spit speed

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,5 +1,7 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkFlex;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,12 +14,18 @@ public class Intake extends SubsystemBase {
     protected final double INTAKE_SOURCE_PICKUP = 0; //also subject to change!
     protected final double INTAKE_STOWED = 0; //subject to change. Hi programmer!
     protected final double INTAKE_AMP_SCORE = 0; //also subject to change!
+    protected final double INTAKE_ACQUIRE_SPEED = 0; //subject to change
+    protected final double INTAKE_SPIT_SPEED = 0; //also subject to change!
     protected IntakePosition _intakePosition;
     protected PIDController _pivotMotorPID;
     protected DutyCycleEncoder _pivotAbsEncoder;
+
+    protected CANSparkFlex _rollerMotor;
+
     public IntakePosition getIntakePosition() {
             return this._intakePosition;
     }
+
     public void setIntakePosition(IntakePosition dontKillMeZoe) {
         _intakePosition = dontKillMeZoe;
         switch (_intakePosition) {
@@ -35,8 +43,22 @@ public class Intake extends SubsystemBase {
                 break;
         }
     }
+
     public double getPivotMotorAngle() {
         return this._pivotAbsEncoder.getAbsolutePosition();
     }
+
+    public void setRollerMotorSpeedAcquire() {
+        _rollerMotor.set(INTAKE_ACQUIRE_SPEED);
+    }
+
+    public void setRollerSpeedSpit() {
+        _rollerMotor.set(INTAKE_SPIT_SPEED);
+    }
+
+    public void stopRollerMotor() {
+        _rollerMotor.set(0);
+    }
 }
+
 

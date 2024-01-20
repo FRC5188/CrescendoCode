2ecb42ffd811e10c4de9d4082040a80dda30b892
collinkn <collinkn@rose-hulman.edu>
2024-01-20 14:29:58 -0500

    sets target position

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -2,17 +2,39 @@ package frc.robot.Subsystems;
 
 import com.revrobotics.CANSparkFlex;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+
+    protected enum ShooterPosition {
+        Subwoofer, Podium
+    }
+
     protected double _targetFlywheelSpeed;
     protected CANSparkFlex _leftFlywheelMotor;
     protected CANSparkFlex _rightFlywheelMotor;
+    protected ShooterPosition _targetPosition;
+    protected final double SUBWOOFER_ANGLE = 0; //subject to change!
+    protected final double PODIUM_ANGLE = 0; //also subject to change
+    protected PIDController _anglePIDController;
 
     public void setFlywheelSpeed(double targetSpeed) {
         _targetFlywheelSpeed = targetSpeed;
         _leftFlywheelMotor.set(targetSpeed);
         _rightFlywheelMotor.set(targetSpeed);
+    }
+
+    public void setTargetPosition(ShooterPosition newPosition) {
+        _targetPosition = newPosition;
+        switch (_targetPosition) {
+            case Subwoofer:
+                _anglePIDController.setSetpoint(SUBWOOFER_ANGLE);
+                break;
+            case Podium:
+                _anglePIDController.setSetpoint(PODIUM_ANGLE);
+                break;
+        }
     }
 
 }

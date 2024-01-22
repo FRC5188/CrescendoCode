a4f3493899575a15dedae78b0fd6ad87683cf0bf
rhit-edgingza <edgingza@rose-hulman.edu>
2024-01-21 21:14:04 -0500

    added basic framework

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,52 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.VortexSwerveCode.subsystems.Swerve;
+import frc.robot.VortexSwerveCode.commands.TeleopSwerve;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
 
-  private void configureBindings() { }
+  /* Controllers */
+    private final Joystick driver = new Joystick(0);
+
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+    private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+
+    /* Subsystems */
+    private final Swerve _swerve = new Swerve();
+
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+      _swerve.setDefaultCommand(
+          new TeleopSwerve(
+              _swerve, 
+              () -> -driver.getRawAxis(translationAxis) * 0.5, 
+              () -> -driver.getRawAxis(strafeAxis) * 0.5, 
+              () -> -driver.getRawAxis(rotationAxis), 
+              () -> robotCentric.getAsBoolean()
+          )
+      );
+
+      configureBindings();
+    }
+
+  private void configureBindings() {
+    /* Driver Buttons */
+    zeroGyro.onTrue(new InstantCommand(() -> _swerve.resetModulesToAbsolute()));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");

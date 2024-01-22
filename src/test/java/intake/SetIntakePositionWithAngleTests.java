fbe1dc60d8a8bc195610796d8ee5154c301f8653
rhit-edgingza <edgingza@rose-hulman.edu>
2024-01-22 08:59:45 -0500

    Added SetIntakeWithAngle tests

--- src/test/java/intake/SetIntakePositionWithAngleTests.java
+++ src/test/java/intake/SetIntakePositionWithAngleTests.java
@@ -40,13 +40,73 @@ public class SetIntakePositionWithAngleTests {
 
     }
 
-    // A test case with hardware mocking
     @Test
     void testSetIntakePositionWithAngle_withMinAngle_expectPIDSetpointSet() {
         double inputAngle = IntakeConstants.MIN_INTAKE_ANGLE;
 
         // We expect this method to set the PID's setpoint to this value
         _hardware.getPivotMotorPID().setSetpoint(IntakeConstants.MIN_INTAKE_ANGLE);
+
+        // Here we call a hardware method called replayHardware()
+        // This causes EasyMock to run all of the stuff we just recorded
+        replayMocks();
+
+        // Here is where we call the method under test
+        _intake.setIntakePositionWithAngle(inputAngle);
+
+        // Here is where we make assertions about behavior and call verifyHardware()
+        // In this test, our assertions are handled by EasyMock, since
+        // we tell it what we expect our motors to output.
+        // So we will only call verifyHardware()
+        verifyMocks();
+    }
+
+    @Test
+    void testSetIntakePositionWithAngle_withMaxAngle_expectPIDSetpointSet() {
+        double inputAngle = IntakeConstants.MAX_INTAKE_ANGLE;
+
+        // We expect this method to set the PID's setpoint to this value
+        _hardware.getPivotMotorPID().setSetpoint(IntakeConstants.MAX_INTAKE_ANGLE);
+
+        // Here we call a hardware method called replayHardware()
+        // This causes EasyMock to run all of the stuff we just recorded
+        replayMocks();
+
+        // Here is where we call the method under test
+        _intake.setIntakePositionWithAngle(inputAngle);
+
+        // Here is where we make assertions about behavior and call verifyHardware()
+        // In this test, our assertions are handled by EasyMock, since
+        // we tell it what we expect our motors to output.
+        // So we will only call verifyHardware()
+        verifyMocks();
+    }
+
+    @Test
+    void testSetIntakePositionWithAngle_withInvalidMaxAngle_expectPIDSetpointToNotChange() {
+        double inputAngle = IntakeConstants.MAX_INTAKE_ANGLE + 0.01;
+
+        // We expect this method to not set the PID's setpoint
+
+        // Here we call a hardware method called replayHardware()
+        // This causes EasyMock to run all of the stuff we just recorded
+        replayMocks();
+
+        // Here is where we call the method under test
+        _intake.setIntakePositionWithAngle(inputAngle);
+
+        // Here is where we make assertions about behavior and call verifyHardware()
+        // In this test, our assertions are handled by EasyMock, since
+        // we tell it what we expect our motors to output.
+        // So we will only call verifyHardware()
+        verifyMocks();
+    }
+
+    @Test
+    void testSetIntakePositionWithAngle_withInvalidMinAngle_expectPIDSetpointToNotChange() {
+        double inputAngle = IntakeConstants.MIN_INTAKE_ANGLE - 0.01;
+
+        // We expect this method to not set the PID's setpoint
 
         // Here we call a hardware method called replayHardware()
         // This causes EasyMock to run all of the stuff we just recorded

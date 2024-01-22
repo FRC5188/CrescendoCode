066c2d87411552ab7d6a50f4b73479b57ff9a82c
rhit-edgingza <edgingza@rose-hulman.edu>
2024-01-22 15:29:23 -0500

    Added rest of hardware

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -5,10 +5,14 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.hardware.intake.IntakeHardware;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
+  private IntakeHardware _hardware;
+
+  public Intake(IntakeHardware hardware) {
+    _hardware = hardware;
+  }
 
   @Override
   public void periodic() {

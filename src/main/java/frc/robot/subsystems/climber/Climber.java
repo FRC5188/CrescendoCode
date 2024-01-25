2bac71ece8459d77bd6d93068a8ebab8df22c350
mitchgres <mgresham4@ivytech.edu>
2024-01-24 20:27:38 -0500

    get and set setCanMove

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -8,11 +8,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.climber.ClimberHardware;
 
 public class Climber extends SubsystemBase {
+
   private ClimberHardware _hardware;
+  private boolean _canMove; 
 
   public Climber(ClimberHardware hardware) {
     _hardware = hardware;
   }
+
+public void setCanMove(boolean canMove) {
+  _canMove = canMove; 
+}
+
 
   @Override
   public void periodic() {

88683f326b7982103e68beb0076cedf91498e088
CrystalWang <crystalwang906@gmail.com>
2024-01-25 19:06:25 -0500

    implemented getCanMove

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.climber;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,13 +5,17 @@ import frc.robot.hardware.climber.ClimberHardware;
 
 public class Climber extends SubsystemBase {
   private ClimberHardware _hardware;
-
+  private boolean _canMove;
   public Climber(ClimberHardware hardware) {
     _hardware = hardware;
+    _canMove = false;
+  }
+
+  public boolean getCanMove() {
+    return _canMove;
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }

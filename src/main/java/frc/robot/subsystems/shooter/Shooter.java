f8908ce002ab932454b0c0b18e0c75c8b25eeed3
CrystalWang <crystalwang906@gmail.com>
2024-01-25 19:39:54 -0500

    implemented configAngleMotor

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,8 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.shooter;
+
+import com.revrobotics.CANSparkFlex;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.shooter.ShooterHardware;
@@ -13,9 +11,11 @@ public class Shooter extends SubsystemBase {
   public Shooter(ShooterHardware hardware) {
     _hardware = hardware;
   }
+  public CANSparkFlex configAngleMotor(CANSparkFlex angleMotor){
+    return angleMotor;
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }

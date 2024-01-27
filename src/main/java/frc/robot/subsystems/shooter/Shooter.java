0adbb307718cf01f8eb5adda0264383bf6eb5bc9
Brady Exoo <bradyallen67@gmail.com>
2024-01-26 20:00:41 -0500

    Begin implementing setFlywheelSpeedRPM(double targetSpeedInRPM)

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -10,43 +10,54 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.shooter.ShooterHardware;
 
 public class Shooter extends SubsystemBase {
-  // Note: The channel that this encoder is on will need to be configured for the
-  // robot.
-  ShooterHardware _hardware;
+    // Note: The channel that this encoder is on will need to be configured for the
+    // robot.
+    ShooterHardware _hardware;
+    private double _flywheelSpeedRPM;
 
-  public Shooter(ShooterHardware hardware) {
-    _hardware = hardware;
-  }
-
-  public void setTargetPositionAsAngle(double angle) {
-    if (angle < ShooterConstants.MIN_SHOOTER_ANGLE) {
-      // TODO: Log invalid angle: Parameter 'angle' must >= MIN_SHOOTER_ANGLE. -KtH
-      // 2024/01/23
-      return;
-
-    } else if (angle > ShooterConstants.MAX_SHOOTER_ANGLE) {
-      // TODO: Log invalid angle: Parameter 'angle' must <= MAX_SHOOTER_ANGLE. -KtH
-      // 2024/01/23
-      return;
-
-    } else {
-      _hardware.getAnglePIDController().setSetpoint(angle);
-      }
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  public double getCurrentPositionInDegrees() throws RuntimeException {
-    double encoderValueAsRotations = _hardware.getAngleEncoder().get();
-    if (encoderValueAsRotations >= ShooterConstants.MAXIMUM_ANGLE_ENCODER_TURNS + Rotation2d.fromDegrees(10).getRotations()
-        || encoderValueAsRotations <= ShooterConstants.MINIMUM_ANGLE_ENCODER_TURNS - Rotation2d.fromDegrees(10).getRotations()) {
-      throw new RuntimeException(
-          "It's impossible for the encoder to be this value. There must be a hardware error. Shut down this subsystem to not break everything.");
-    } else {
-      return Rotation2d.fromRotations(encoderValueAsRotations).getDegrees();
+    public Shooter(ShooterHardware hardware) {
+        _hardware = hardware;
     }
-  }
+
+    public void setTargetPositionAsAngle(double angle) {
+        if (angle < ShooterConstants.MIN_SHOOTER_ANGLE) {
+            // TODO: Log invalid angle: Parameter 'angle' must >= MIN_SHOOTER_ANGLE. -KtH
+            // 2024/01/23
+            return;
+
+        } else if (angle > ShooterConstants.MAX_SHOOTER_ANGLE) {
+            // TODO: Log invalid angle: Parameter 'angle' must <= MAX_SHOOTER_ANGLE. -KtH
+            // 2024/01/23
+            return;
+
+        } else {
+            _hardware.getAnglePIDController().setSetpoint(angle);
+        }
+    }
+
+    // TODO: Make this multiply by gear ratio
+    public void setFlywheelSpeedRPM(double targetSpeedInRPM){
+        _flywheelSpeedRPM = targetSpeedInRPM;
+        _hardware.getTopFlywheelMotor().set(targetSpeedInRPM * 1.00);
+        _hardware.getBottomFlywheelMotor().set(targetSpeedInRPM * 1.00);
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    public double getCurrentPositionInDegrees() throws RuntimeException {
+        double encoderValueAsRotations = _hardware.getAngleEncoder().get();
+        if (encoderValueAsRotations >= ShooterConstants.MAXIMUM_ANGLE_ENCODER_TURNS
+                + Rotation2d.fromDegrees(10).getRotations()
+                || encoderValueAsRotations <= ShooterConstants.MINIMUM_ANGLE_ENCODER_TURNS
+                        - Rotation2d.fromDegrees(10).getRotations()) {
+            throw new RuntimeException(
+                    "It's impossible for the encoder to be this value. There must be a hardware error. Shut down this subsystem to not break everything.");
+        } else {
+            return Rotation2d.fromRotations(encoderValueAsRotations).getDegrees();
+        }
+    }
 }

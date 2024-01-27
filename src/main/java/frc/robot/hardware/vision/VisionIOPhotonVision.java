3caa2975646e0887fd18fbe67fe9aa0716026f0d
first5188 <first5188@gmail.com>
2024-01-27 15:22:14 -0500

    Updated Vision Inputs -- Java Streams :D

--- src/main/java/frc/robot/hardware/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/robot/hardware/vision/VisionIOPhotonVision.java
@@ -1,11 +1,14 @@
 package frc.robot.hardware.vision;
 
+import java.util.Comparator;
 import java.util.List;
 //import java.util.Optional;
+import java.util.Optional;
 
 //import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
 //import org.photonvision.PhotonTargetSortMode;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -14,6 +17,8 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.hardware.HardwareConstants;
 import frc.robot.hardware.vision.VisionIO.VisionIOInputs;
 
@@ -47,22 +52,89 @@ public class VisionIOPhotonVision {
     private List<PhotonTrackedTarget> _cameraOneTargets;
     private List<PhotonTrackedTarget> _cameraTwoTargets;
 
+    private Pose3d _cameraOneEstimatedPose;
+    private Pose3d _cameraTwoEstimatedPose;
+
     private boolean _doesCameraOneHaveTarget = false;
     private boolean _doesCameraTwoHaveTarget = false;
+
+    private SwerveDrivePoseEstimator _poseEstimatorFromOdometry;
 
     public void updateInputs(VisionIOInputs inputs) {
         PhotonPipelineResult cameraOneResult = CAMERA_ONE.getLatestResult();
         PhotonPipelineResult cameraTwoResult = CAMERA_TWO.getLatestResult();
 
+        _cameraOneTimestamp = Timer.getFPGATimestamp();
+        _cameraTwoTimestamp = Timer.getFPGATimestamp();
+
         _cameraOneTargets = cameraOneResult.getTargets();
         _cameraTwoTargets = cameraTwoResult.getTargets();
 
-        //_doesCameraOneHaveTarget = checkValidResult(_cameraOneTargets) && _cameraOneTargets
+        _doesCameraOneHaveTarget = checkValidResult(_cameraOneTargets) && !_cameraOneTargets.isEmpty();
+        _doesCameraTwoHaveTarget = checkValidResult(_cameraTwoTargets) && !_cameraTwoTargets.isEmpty();
+
+        if (_doesCameraOneHaveTarget && _doesCameraTwoHaveTarget) {
+            // We can use both cameras for our calculations
+            _cameraOneEstimatedPose = CAMERA_ONE_ESTIMATION.update().get().estimatedPose;
+            _cameraTwoEstimatedPose = CAMERA_TWO_ESTIMATION.update().get().estimatedPose;
+            _poseEstimatorFromOdometry.addVisionMeasurement(
+                _cameraOneEstimatedPose.toPose2d(), 
+                _cameraOneTimestamp);
+
+            _poseEstimatorFromOdometry.addVisionMeasurement(
+                _cameraTwoEstimatedPose.toPose2d(), 
+                _cameraTwoTimestamp);
+        }
+        else if (_doesCameraOneHaveTarget){
+            // Then we'll just use camera one.
+            _poseEstimatorFromOdometry.addVisionMeasurement(
+                _cameraOneEstimatedPose.toPose2d(), 
+                _cameraOneTimestamp);
+        }
+        else if (_doesCameraTwoHaveTarget){
+            // Then we'll just use camera two.
+            _poseEstimatorFromOdometry.addVisionMeasurement(
+                _cameraTwoEstimatedPose.toPose2d(), 
+                _cameraTwoTimestamp);
+        }
+        else {
+            // We can't use either. I don't know fix it. 
+            // Right now we'll just not do anything and hope it works :).
+        }
+
+        inputs._cameraOneX = (_doesCameraOneHaveTarget) ? (_cameraOneEstimatedPose.getX()) : (-1.0);
+        inputs._cameraOneY = (_doesCameraOneHaveTarget) ? (_cameraOneEstimatedPose.getY()) : (-1.0);
+        inputs._cameraOneTimestamp = _cameraOneTimestamp; 
+        inputs._cameraOneMaxAmbiguity = _cameraOneTargets.stream().max(
+            (a, b) -> Double.compare(a.getPoseAmbiguity(), b.getPoseAmbiguity()))
+            .get().getPoseAmbiguity();
+        inputs._camerOneMaxDistance = _cameraOneTargets.stream().max(
+            (a, b) -> Double.compare(a.getBestCameraToTarget().getTranslation().getNorm(), b.getBestCameraToTarget().getTranslation().getNorm()))
+            .get().getPoseAmbiguity();
+        inputs. _cameraOneHasTarget = this._doesCameraOneHaveTarget;
+
+        inputs._cameraTwoX = (_doesCameraTwoHaveTarget) ? (_cameraTwoEstimatedPose.getX()) : (-1.0);
+        inputs._cameraTwoY = (_doesCameraTwoHaveTarget) ? (_cameraTwoEstimatedPose.getY()) : (-1.0);
+        inputs._cameraTwoTimestamp = _cameraTwoTimestamp;
+        inputs._cameraTwoMaxAmbiguity = _cameraTwoTargets.stream().max(
+            (a, b) -> Double.compare(a.getPoseAmbiguity(), b.getPoseAmbiguity()))
+            .get().getPoseAmbiguity(); // TODO: Not sure if this actually gets the max of the list but we'll see :).
+        inputs._cameraTwoMaxDistance = _cameraTwoTargets.stream().max(
+            (a, b) -> Double.compare(a.getBestCameraToTarget().getTranslation().getNorm(), b.getBestCameraToTarget().getTranslation().getNorm()))
+            .get().getPoseAmbiguity();
+        inputs._cameraTwoHasTarget = this._doesCameraTwoHaveTarget;
+
+        inputs._combinedX = _poseEstimatorFromOdometry.getEstimatedPosition().getX();
+        inputs._combinedY = _poseEstimatorFromOdometry.getEstimatedPosition().getY();
     }
 
-    public void setOdometryReferenceEstimator(SwerveDrivePoseEstimator inputEstimationFromOdometry) {
+    // Note: This has to be run in the periodic() for Odometry. 
+    public void setOdometryReferenceEstimator(SwerveDrivePoseEstimator poseEstimatorFromOdometry) {
+        this._poseEstimatorFromOdometry = poseEstimatorFromOdometry;
+    }
 
-    } 
+    // Note: This must be run after you set the new estimation and update inputs. 
+    public SwerveDrivePoseEstimator getUpdatedEstimation(){ return this._poseEstimatorFromOdometry; }
 
     private boolean checkValidResult(List<PhotonTrackedTarget> result) {
         for (PhotonTrackedTarget target : result) {

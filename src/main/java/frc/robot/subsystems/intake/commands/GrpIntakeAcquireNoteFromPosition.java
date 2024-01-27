e3fc93bb33b9711b9975166534570467df3c38ac
chrstne-ii <95193180+chrstne-ii@users.noreply.github.com>
2024-01-27 16:23:55 -0500

    made changes to GrpIntakeAcquireNoteFromPosition

--- src/main/java/frc/robot/subsystems/intake/commands/GrpIntakeAcquireNoteFromPosition.java
+++ src/main/java/frc/robot/subsystems/intake/commands/GrpIntakeAcquireNoteFromPosition.java
@@ -7,17 +7,19 @@ import frc.robot.subsystems.intake.Intake.IntakePosition;
 public class GrpIntakeAcquireNoteFromPosition extends SequentialCommandGroup {
     private Intake _intakeSubsystem;
     private IntakePosition _intakePosition;
+    private int _timeoutInMs;
 
-    public GrpIntakeAcquireNoteFromPosition(Intake intakeSubsystem, IntakePosition intakePosition) {
+    public GrpIntakeAcquireNoteFromPosition(Intake intakeSubsystem, IntakePosition intakePosition, int timeoutInMs) {
         _intakeSubsystem = intakeSubsystem;
         _intakePosition = intakePosition;
+        _timeoutInMs = timeoutInMs;
 
         addRequirements(_intakeSubsystem);
 
         addCommands(
                 new GrpIntakeMoveToPosition(intakeSubsystem, intakePosition),
                 new CmdIntakeStartRollersAcquire(intakeSubsystem),
-                new CmdIntakeWaitForNote(0, intakeSubsystem),
+                new CmdIntakeWaitForNote(_timeoutInMs, intakeSubsystem),
                 new CmdIntakeStopRollers(intakeSubsystem),
                 new GrpIntakeMoveToPosition(intakeSubsystem, _intakePosition.Stowed));
     }

d99971c783a0241279e2aead421d95f4ff09404c
shlap <Chrispy1729@gmail.com>
2024-01-29 18:29:45 -0500

    Added a setter to setAutoShootEnabled

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -10,10 +10,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase {
   // Note: The channel that this encoder is on will need to be configured for the
   // robot.
-  //ShooterHardware _hardware;
+  // ShooterHardware _hardware;
 
   // public Shooter(ShooterHardware hardware) {
-  //   _hardware = hardware;
+  // _hardware = hardware;
   // }
   private static boolean _autoShootEnabled = true;
 
@@ -28,9 +28,9 @@ public class Shooter extends SubsystemBase {
       // 2024/01/23
       return;
 
-    } //else {
-      //_hardware.getAnglePIDController().setSetpoint(angle);
-     // }
+    } // else {
+      // _hardware.getAnglePIDController().setSetpoint(angle);
+      // }
   }
 
   @Override
@@ -39,16 +39,23 @@ public class Shooter extends SubsystemBase {
   }
 
   // public double getCurrentPositionInDegrees() throws RuntimeException {
-  //   double encoderValueAsRotations = _hardware.getAngleEncoder().get();
-  //   if (encoderValueAsRotations >= ShooterConstants.MAXIMUM_ANGLE_ENCODER_TURNS + Rotation2d.fromDegrees(10).getRotations()
-  //       || encoderValueAsRotations <= ShooterConstants.MINIMUM_ANGLE_ENCODER_TURNS - Rotation2d.fromDegrees(10).getRotations()) {
-  //     throw new RuntimeException(
-  //         "It's impossible for the encoder to be this value. There must be a hardware error. Shut down this subsystem to not break everything.");
-  //   } else {
-  //     return Rotation2d.fromRotations(encoderValueAsRotations).getDegrees();
-  //   }
+  // double encoderValueAsRotations = _hardware.getAngleEncoder().get();
+  // if (encoderValueAsRotations >= ShooterConstants.MAXIMUM_ANGLE_ENCODER_TURNS +
+  // Rotation2d.fromDegrees(10).getRotations()
+  // || encoderValueAsRotations <= ShooterConstants.MINIMUM_ANGLE_ENCODER_TURNS -
+  // Rotation2d.fromDegrees(10).getRotations()) {
+  // throw new RuntimeException(
+  // "It's impossible for the encoder to be this value. There must be a hardware
+  // error. Shut down this subsystem to not break everything.");
+  // } else {
+  // return Rotation2d.fromRotations(encoderValueAsRotations).getDegrees();
   // }
-    public boolean isAutoShootEnabled() {
-      return _autoShootEnabled;
-    }
+  // }
+  public boolean isAutoShootEnabled() {
+    return _autoShootEnabled;
+  }
+
+  public void setAutoShootEnabled(boolean enabled) {
+    _autoShootEnabled = enabled;
+  }
 }

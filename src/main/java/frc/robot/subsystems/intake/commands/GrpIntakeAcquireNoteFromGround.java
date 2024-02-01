d6273d861d642d9a3460353e3cfa8c8d293bc918
chrstne-ii <95193180+chrstne-ii@users.noreply.github.com>
2024-02-01 00:18:11 -0500

    Implemented GrpIntakeAcquireNoteFrom[] for Source and Grnd Pickup

---
+++ src/main/java/frc/robot/subsystems/intake/commands/GrpIntakeAcquireNoteFromGround.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.Intake.IntakePosition;
+
+public class GrpIntakeAcquireNoteFromGround extends SequentialCommandGroup {
+    private Intake _intakeSubsystem;
+    private IntakePosition _intakePosition;
+    private int _timeoutInMs;
+
+    public GrpIntakeAcquireNoteFromGround(Intake intakeSubsystem, IntakePosition intakePosition, int timeoutInMs) {
+        addRequirements(intakeSubsystem);
+
+        addCommands(
+                new GrpIntakeAcquireNoteFromPosition(intakeSubsystem, IntakePosition.GroundPickup, timeoutInMs));
+    }
+}

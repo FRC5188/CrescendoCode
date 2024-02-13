727d51e9e14bbf4d728510b1e71a65fa3ce0b4c2
Garrett Hart <hartgl@rose-hulman.edu>
2024-02-12 19:29:30 -0500

    add contants to for visualizer

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeConstants {
+    public static final double INTAKE_WIDTH = 0;
+    public static final double INTAKE_HEIGHT = 2;
+    public static final double INTAKE_LENGTH = 3;
     // Intake Range Values
     public static double MIN_INTAKE_ANGLE = 0.0;
     public static double MAX_INTAKE_ANGLE = 160.0;

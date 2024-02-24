4e69a7804ea0929b2c2c3f5731fde3e1015ce3dc
Mitchell <mgresham4@ivytech.edu>
2024-02-24 14:16:13 -0500

    Example of ShooterCommandFactory

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterCommandFactory.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+
+class ShooterCommandFactory {
+    private Shooter _shooter;
+
+    protected ShooterCommandFactory(Shooter shooter) {
+        this._shooter = shooter;
+    }
+
+    /** Sets the angle of the Shooter. */
+    protected Command setAngle(double angle) {
+        return new InstantCommand(() -> 
+            _shooter.setTargetPositionAsAngle(angle), 
+            _shooter);
+    }
+
+    protected Command adjustAngle(double angle) {
+        return new InstantCommand(() -> 
+            _shooter.setTargetPositionAsAngle(_shooter.getCurrentZone().getShooterAngle() + angle), 
+            _shooter);
+    }
+}

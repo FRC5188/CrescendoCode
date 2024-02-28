1b75877a47010eba7bb313e6089ba7c4a11ef692
Garrett Hart <hartgl@rose-hulman.edu>
2024-02-28 14:51:14 -0500

    turn off the cameras for vision rn. they arent mounted correctly

--- src/main/java/frc/robot/HardwareConstants.java
+++ src/main/java/frc/robot/HardwareConstants.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
 public class HardwareConstants {
-    public static final int NUMBER_OF_CAMERAS = 2;
+    public static final int NUMBER_OF_CAMERAS = 0;
     public class CanIds {
         
         // |====================== INTAKE SUBSYSTEM CAN IDs ======================|

7cdc8815e9d164d36210d8c5a00c543208ff2a74
kt-h <113950897+kt-h@users.noreply.github.com>
2024-02-27 19:14:20 -0500

    fixed :(

--- src/main/java/frc/robot/subsystems/visiondrive/RealVisionDriveIO.java
+++ src/main/java/frc/robot/subsystems/visiondrive/RealVisionDriveIO.java
@@ -1,12 +1,51 @@
 package frc.robot.subsystems.visiondrive;
 
-import frc.robot.HardwareConstants;
+import org.photonvision.PhotonUtils;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class RealVisionDriveIO implements VisionDriveIO {
 
-    private PIDController _translatePID;
+    final double CAMERA_HEIGHT_IN_METERS = 0.635;
 
-    public void updateInputs(VisionIOInputs inputs) {
-    
+    final double CAMERA_PITCH_RADIANS = 20.0 / 180 * (3.14159);
+
+    final double GOAL_RANGE_METERS = 0.1;
+
+    private PIDController _translatePID;
+    private PIDController _rotatePID;
+
+    private double _tx;
+    private double _ty;
+
+    private NetworkTable _table;
+
+    public RealVisionDriveIO() {
+        _translatePID = new PIDController(0.1, 0, 0.05);
+        _rotatePID = new PIDController(7.0, 0, 0.05);
+
+        _table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public boolean hasTarget() {
+        return 1.0 == _table.getEntry("tv").getDouble(0.0);
+    }
+
+    public void updateInputs(VisionDriveIOInputs inputs) {
+        if (hasTarget()) {
+            _tx = _table.getEntry("tx").getDouble(100.0);
+            _ty = _table.getEntry("ty").getDouble(100.0);
+
+            inputs._range = PhotonUtils.calculateDistanceToTargetMeters(
+                    CAMERA_HEIGHT_IN_METERS,
+                    0.0,
+                    CAMERA_PITCH_RADIANS,
+                    _ty);
+            
+            inputs._forwardSpeed = -_translatePID.calculate(inputs._range, GOAL_RANGE_METERS);
+            inputs._rotSpeed = -_rotatePID.calculate(_tx, 0);
+        }
     }
 }
--- src/main/java/frc/robot/subsystems/visiondrive/VisionDriveIO.java
+++ src/main/java/frc/robot/subsystems/visiondrive/VisionDriveIO.java
@@ -2,15 +2,16 @@ package frc.robot.subsystems.visiondrive;
 
 import org.littletonrobotics.junction.AutoLog;
 
-import frc.robot.HardwareConstants;
-
 public interface VisionDriveIO {
     
     @AutoLog
     public static class VisionDriveIOInputs {
-        public double _setSpeed;
-        public double _rot;
+        public double _forwardSpeed;
+        public double _rotSpeed;
+        public double _range;
     }
 
-    public default void updateInputs(VisionDriveIOInputs inputs) {}
+    public void updateInputs(VisionDriveIOInputs inputs);
+
+    public boolean hasTarget();
 }

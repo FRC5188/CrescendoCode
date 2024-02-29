de7356ec1cf809150547d9decd7dc35226f9c6d9
area5188 <bradyallen67@gmail.com>
2024-02-28 20:24:15 -0500

    reverted back to earlier offsets

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -40,9 +40,9 @@ public class DriveConstants {
      * 
      */
     
-    public static final double FL_OFFSET = 0.0588;
-    public static final double FR_OFFSET = 0.2110;
-    public static final double BL_OFFSET = -0.2687;
-    public static final double BR_OFFSET = 0.1160;
+    public static final double FL_OFFSET = -0.453711;
+    public static final double FR_OFFSET = 0.716553;
+    public static final double BL_OFFSET = 0.230787455240885;
+    public static final double BR_OFFSET = -0.380286458333333;
 
 }

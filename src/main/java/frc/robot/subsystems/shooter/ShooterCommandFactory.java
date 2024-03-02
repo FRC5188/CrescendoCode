901b9563dc29b97cfe7876d92615055598f8169a
Mackenzie Racoop <mackenziejeeyoung@gmail.com>
2024-03-02 11:34:14 -0500

    made commands public

--- src/main/java/frc/robot/subsystems/shooter/ShooterCommandFactory.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterCommandFactory.java
@@ -14,41 +14,41 @@ public class ShooterCommandFactory {
     private ShooterZone _zone;
     private boolean _enabled;
 
-    protected ShooterCommandFactory(Shooter shooter, Shooter shooterSubsystem, Intake intakeSubsystem, ShooterZone zone, boolean _enabled) {
+    public ShooterCommandFactory(Shooter shooter, Shooter shooterSubsystem, Intake intakeSubsystem, ShooterZone zone, boolean _enabled) {
         this._shooter = shooter;
         this._shooterSubsystem = shooterSubsystem;
-        this._intakeSubsystem = intakeSubsystem;
+        this._intakeSubsystem = intakeSubsystem
         this._zone = zone;
         this._enabled = enabled;
     }
 
     /** Sets the angle of the Shooter. */
-    protected Command setAngle(double angle) {
+    public Command setAngle(double angle) {
         return new InstantCommand(() -> 
             _shooter.setTargetPositionAsAngle(angle), 
             _shooter);
     }
 
-    protected Command adjustAngle(double angle) {
+    public Command adjustAngle(double angle) {
         return new InstantCommand(() -> 
             _shooter.setTargetPositionAsAngle(_shooter.getCurrentZone().getShooterAngle() + angle), 
             _shooter);
     }
 
-    protected Command runFlywheelsForZone() {
+    public Command runFlywheelsForZone() {
         return StartEndCommand(() ->
         // TODO
         )
     }
 
-    protected Command runPIDs() {
+    public Command runPIDs() {
         return new InstantCommand(() ->
         _shooterSubsystem.runAnglePid(),
         _shooterSubsystem
         );
     }
 
-    protected Command runForZone(ShooterZone zone) {
+    public Command runForZone(ShooterZone zone) {
         return new InstantCommand (() ->
         _shooterSubsystem.runShooterForZone(_zone),
         _shooterSubsystem
@@ -56,21 +56,21 @@ public class ShooterCommandFactory {
 
     }
 
-    protected Command setAutoShootEnabled(boolean enabled) {
+    public Command setAutoShootEnabled(boolean enabled) {
         return new InstantCommand (() ->
         _shooterSubsystem.setAutoShootEnabled(_enabled),
         _shooterSubsystem
         );
     }
 
-    protected Command setPositionByZone() {
+    public Command setPositionByZone() {
         return new InstantCommand (() ->
         _shooterSubsystem.setShooterPositionWithZone(_zone),
         _shooterSubsystem
         );
     }
 
-     protected Command waitUntilReady() {
+     public Command waitUntilReady() {
         return new Command() {
             @Override
             public boolean isFinished() {

31764fd756777991609a5df09ffbb674a09df32d
Mackenzie Racoop <mackenziejeeyoung@gmail.com>
2024-03-02 16:41:26 -0500

    merge conflicts resolved!!! (thank you garrett)

--- src/main/java/frc/robot/subsystems/intake/IntakeCommandFactory.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeCommandFactory.java
@@ -8,7 +8,6 @@ import frc.robot.subsystems.intake.Intake.IntakePosition;
 import frc.robot.subsystems.intake.commands.CmdIntakeWaitForNote;
 import frc.robot.subsystems.intake.commands.CmdAquireNoteFor;
 
-
 public class IntakeCommandFactory {
     private Intake _intake;
 
@@ -16,18 +15,14 @@ public class IntakeCommandFactory {
         this._intake = intake;
     }
 
-
     /** Turns ON the ROLLERS in the direction toward the robot. Doesn't automatically turn off. */
-
     public Command aquire() {
         return new InstantCommand(
             this._intake::setRollerMotorSpeedAcquire,
             this._intake);
     }
 
-
     /** Turns ON the ROLLERS away from the robot. Doesn't automatically turn off. */
-
     public Command spit(double timeSeconds) {
 
         return new StartEndCommand(
@@ -40,53 +35,29 @@ public class IntakeCommandFactory {
          this._intake).withTimeout(timeSeconds);
     }
 
-
     /** Should be called every cycle. This should be the default command on Intake. */
-
     public Command runPID() {
         return new RunCommand(
             this._intake::runPivotPID, this._intake);
     }
 
-
     /** Sets the positions of the PID for the Intake. */
-
     public Command setPosition(Intake.IntakePosition position) {
         return new InstantCommand(
             () -> {
                 this._intake.setIntakePosition(position);
-
                 // We'll turn OFF the ROLLERS if we're stowing.
-
                 if (position == Intake.IntakePosition.Stowed) {
                     this._intake.stopRollerMotor();
                 }
             }, this._intake);
     }
 
-
     /** Turns OFF the ROLLERS. */
-
     public Command stop(){
         return new InstantCommand(
             this._intake::stopRollerMotor, this._intake);
     }
-
-
-    public Command waitForIntake() {
-        return new RunCommand(() -> {}, this._intake).until(() -> this._intake.pivotAtSetpoint());
-    }
-
-    public Command waitForNote(double timeSeconds) {
-        return new StartEndCommand(
-         this._intake::setRollerMotorSpeedSpit,
-         () -> {
-                this._intake.stopRollerMotor();
-                this._intake.resetHasNote();
-
-         }, this._intake)
-            .withTimeout(timeSeconds)
-            .until(() -> this._intake.hasNote());
 
     /** Will SET THE POSITION for the intake then as it moves to the position will turn ON the ROLLERS.
      * Then it'll wait until it's been given a note based on current cutoff, run for an additional 150ms, then stow.
@@ -99,7 +70,6 @@ public class IntakeCommandFactory {
             .andThen(new CmdIntakeWaitForNote(0, this._intake))
             .andThen(new CmdAquireNoteFor(150, _intake))
             .andThen(this.setPosition(IntakePosition.Stowed));
-
     }
 
     public Command pickUpFromGround() {
@@ -110,9 +80,7 @@ public class IntakeCommandFactory {
         return this.pickUpNoteFrom(Intake.IntakePosition.SourcePickup);
     }
 
-
     public Command aquire(int timeMS) {
         return new CmdAquireNoteFor(timeMS, this._intake);
     }
-
 }

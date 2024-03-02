a33a112f27edf402259c8f6ebe7a9aa22ab5a346
Mackenzie Racoop <mackenziejeeyoung@gmail.com>
2024-03-02 13:00:15 -0500

    reformatted to fit new conventions, runFlywheelsForZone and waitUntilReady still in progress

--- src/main/java/frc/robot/subsystems/shooter/ShooterCommandFactory.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterCommandFactory.java
@@ -10,74 +10,78 @@ import frc.robot.subsystems.intake.Intake.IntakePosition;
 public class ShooterCommandFactory {
     private Shooter _shooter;
     private Shooter _shooterSubsystem;
-    private Intake _intakeSubsystem;
     private ShooterZone _zone;
+    private Intake _intakeSubsystem;
     private boolean _enabled;
 
-    public ShooterCommandFactory(Shooter shooter, Shooter shooterSubsystem, Intake intakeSubsystem, ShooterZone zone, boolean _enabled) {
+    public ShooterCommandFactory(Shooter shooter, Shooter shooterSubsystem, ShooterZone zone, Intake intakeSubsystem, boolean enabled) {
         this._shooter = shooter;
         this._shooterSubsystem = shooterSubsystem;
-        this._intakeSubsystem = intakeSubsystem
         this._zone = zone;
+        this._intakeSubsystem = intakeSubsystem;
         this._enabled = enabled;
     }
 
     /** Sets the angle of the Shooter. */
     public Command setAngle(double angle) {
         return new InstantCommand(() -> 
-            _shooter.setTargetPositionAsAngle(angle), 
-            _shooter);
+            this._shooter.setTargetPositionAsAngle(angle), 
+            this._shooter);
     }
 
     public Command adjustAngle(double angle) {
         return new InstantCommand(() -> 
-            _shooter.setTargetPositionAsAngle(_shooter.getCurrentZone().getShooterAngle() + angle), 
-            _shooter);
+            this._shooter.setTargetPositionAsAngle(_shooter.getCurrentZone().getShooterAngle() + angle), 
+            this._shooter);
     }
 
-    public Command runFlywheelsForZone() {
-        return StartEndCommand(() ->
-        // TODO
-        )
-    }
+    // HELP
+    // public Command runFlywheelsForZone(double speedInRPM) {
+    //     return StartEndCommand(
+    //     this._shooterSubsystem.setFlywheelSpeed(_zone.getLeftFlywheelSpeed()),
+    //     () -> {
+    //         this._shooterSubsystem.stopFlywheels();
+    //     }, this._shooterSubsystem)
+
+    // }
 
     public Command runPIDs() {
-        return new InstantCommand(() ->
-        _shooterSubsystem.runAnglePid(),
-        _shooterSubsystem
+        return new InstantCommand(
+        this._shooterSubsystem::runAnglePid,
+        this._shooterSubsystem
         );
     }
 
     public Command runForZone(ShooterZone zone) {
         return new InstantCommand (() ->
-        _shooterSubsystem.runShooterForZone(_zone),
-        _shooterSubsystem
+        this._shooterSubsystem.runShooterForZone(_zone),
+        this._shooterSubsystem
         );
 
     }
 
     public Command setAutoShootEnabled(boolean enabled) {
-        return new InstantCommand (() ->
-        _shooterSubsystem.setAutoShootEnabled(_enabled),
-        _shooterSubsystem
-        );
+        return new InstantCommand(() ->
+        this._shooterSubsystem.setAutoShootEnabled(_enabled),
+        this._shooterSubsystem);
     }
 
     public Command setPositionByZone() {
         return new InstantCommand (() ->
-        _shooterSubsystem.setShooterPositionWithZone(_zone),
-        _shooterSubsystem
+        this._shooterSubsystem.setShooterPositionWithZone(_zone),
+        this._shooterSubsystem
         );
     }
 
-     public Command waitUntilReady() {
-        return new Command() {
-            @Override
-            public boolean isFinished() {
-                return _shooterSubsystem.isReady() && 
-        _intakeSubsystem.pivotAtSetpoint() && 
-        _intakeSubsystem.getIntakePosition() == IntakePosition.SpeakerScore;
-            }
-        };
-    }
+   // HELP
+    //  public Command waitUntilReady() {
+    //     return new Command() {
+    //         @Override
+    //         public boolean isFinished() {
+    //             return this._shooterSubsystem.isReady() && 
+    //             this._intakeSubsystem.pivotAtSetpoint() && 
+    //             this._intakeSubsystem.getIntakePosition() == IntakePosition.SpeakerScore;
+    //         }
+    //     };
+    // }
 }

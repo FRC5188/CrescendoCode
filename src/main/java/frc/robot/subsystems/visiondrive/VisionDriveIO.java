0f251effab4385379c8f7f5e2fbaa9b94ad167d0
kt-h <113950897+kt-h@users.noreply.github.com>
2024-03-05 19:14:42 -0500

    Doc stringssss

--- src/main/java/frc/robot/subsystems/visiondrive/VisionDriveIO.java
+++ src/main/java/frc/robot/subsystems/visiondrive/VisionDriveIO.java
@@ -4,10 +4,18 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public interface VisionDriveIO {
     
+    /**
+     * If a note is detected, get x and y of the note relative to the crosshair on the screen.
+     * Calculate forward and rotation speeds using PIDs. Return a ChassisSpeeds object. 
+     * If a note is not detected, the speeds will be set to 0.0.
+     */
     public default ChassisSpeeds getChassisSpeeds() {
         return new ChassisSpeeds();
     }
 
+    /**
+     * Returns 1.0 if the Limelight recognizes a target. Returns 0.0 otherwise.
+     */
     public default boolean hasTarget() {
         return false;
     }

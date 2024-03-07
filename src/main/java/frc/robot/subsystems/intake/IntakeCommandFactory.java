2e599947ab405d727f48ff95f270e7f2b587398a
Garrett Hart <hartgl@rose-hulman.edu>
2024-03-06 20:11:39 -0500

    make the build pass checkstyle

--- src/main/java/frc/robot/subsystems/intake/IntakeCommandFactory.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeCommandFactory.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;

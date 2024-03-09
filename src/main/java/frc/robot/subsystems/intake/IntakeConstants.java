afbfee95b3e7177ab31a0a38a91c882465c21a55
Mitchell <mgresham4@ivytech.edu>
2024-03-09 14:33:20 -0500

    Testing Spacing on Dashboard

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.intake;
 
+import org.littletonrobotics.junction.Logger;
+
 public class IntakeConstants {
+
     /************************
      * 
      * INTAKE PID CONSTANTS

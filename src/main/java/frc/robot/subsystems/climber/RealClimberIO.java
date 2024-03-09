f2b4b4cf2d5e4cd41d6893bab0f16d00581dc40d
rhit-edgingza <edgingza@rose-hulman.edu>
2024-03-09 16:14:28 -0500

    Changed Climber SparkFlexes to SparkMaxes

--- src/main/java/frc/robot/subsystems/climber/RealClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/RealClimberIO.java
@@ -1,14 +1,13 @@
 package frc.robot.subsystems.climber;
 
-import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import frc.robot.HardwareConstants;
-import frc.robot.util.MotorFrameConfigurator;
 
 public class RealClimberIO implements ClimberIO {
-    private CANSparkFlex _leftClimberMotor;
-    private CANSparkFlex _rightClimberMotor;
+    private CANSparkMax _leftClimberMotor;
+    private CANSparkMax _rightClimberMotor;
 
     public RealClimberIO() {
         configureLeftClimberMotor();
@@ -41,13 +40,13 @@ public class RealClimberIO implements ClimberIO {
 
     private void configureLeftClimberMotor() {
         // While this method does end up setting the motor equal to itself it is better than having to make a seperate local copy of the motor then returning it. 
-        _leftClimberMotor = new CANSparkFlex(HardwareConstants.CanIds.LEFT_CLIMBER_MOTOR, MotorType.kBrushless);
+        _leftClimberMotor = new CANSparkMax(HardwareConstants.CanIds.LEFT_CLIMBER_MOTOR, MotorType.kBrushless);
         
         _leftClimberMotor.setCANTimeout(100);
         _leftClimberMotor.setSmartCurrentLimit(40);
         _leftClimberMotor.setSecondaryCurrentLimit(55); // This will use an on/off switch to limit current. Not smart, but if we get above 55 amps we have a problem.
 
-        MotorFrameConfigurator.configNoSensor(_leftClimberMotor);
+        // MotorFrameConfigurator.configNoSensor(_leftClimberMotor);
 
         _leftClimberMotor.enableVoltageCompensation(12); // Even if the battery isn't 12V we'll compensate for it.
 
@@ -55,13 +54,13 @@ public class RealClimberIO implements ClimberIO {
     }
 
     private void configureRightClimberMotor() {
-        _rightClimberMotor = new CANSparkFlex(HardwareConstants.CanIds.LEFT_CLIMBER_MOTOR, MotorType.kBrushless);
+        _rightClimberMotor = new CANSparkMax(HardwareConstants.CanIds.LEFT_CLIMBER_MOTOR, MotorType.kBrushless);
         
         _rightClimberMotor.setCANTimeout(100);
         _rightClimberMotor.setSmartCurrentLimit(40);
         _rightClimberMotor.setSecondaryCurrentLimit(55); // This will use an on/off switch to limit current. Not smart, but if we get above 55 amps we have a problem.
 
-        MotorFrameConfigurator.configNoSensor(_rightClimberMotor);
+        // MotorFrameConfigurator.configNoSensor(_rightClimberMotor);
 
         _rightClimberMotor.enableVoltageCompensation(12); // Even if the battery isn't 12V we'll compensate for it.
 

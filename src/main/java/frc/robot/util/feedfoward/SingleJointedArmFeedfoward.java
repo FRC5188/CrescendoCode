0050fe299c65c6c1db2d19c63a96c3021e7e22d9
Mitchell <mgresham4@ivytech.edu>
2024-03-12 18:28:50 -0400

    Updated Uneeded Imports

--- src/main/java/frc/robot/util/feedfoward/SingleJointedArmFeedfoward.java
+++ src/main/java/frc/robot/util/feedfoward/SingleJointedArmFeedfoward.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.system.LinearSystemLoop;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Unit;
 
 public class SingleJointedArmFeedfoward {
   private static final boolean USES_ESTIMATED_VALUES = true;

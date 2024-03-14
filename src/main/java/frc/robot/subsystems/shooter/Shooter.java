25b104024c947972392d2e2b314b24683fd1c887
rhit-edgingza <edgingza@rose-hulman.edu>
2024-03-14 12:13:00 -0400

    Fixed checkstyle

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -9,13 +9,10 @@ import java.util.Map;
 
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.feedfoward.SingleJointedArmFeedforward;
 
 public class Shooter extends SubsystemBase {
     public enum ShooterZone {
